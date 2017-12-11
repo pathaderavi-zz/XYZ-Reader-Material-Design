7d46719834bb4fc7651a0f9e9eb4563e340cc97e
Ravikiran Pathade <pathaderavi@gmail.com>
2017-12-11 00:46:49 -0500

    For Review 2

--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
@@ -17,6 +17,7 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.ShareCompat;
 import android.support.v7.graphics.Palette;
 import android.text.Html;
@@ -283,6 +284,8 @@ public class ArticleDetailFragment extends Fragment implements
         }
 
         bindViews();
+        final Snackbar snackbar = Snackbar.make(mRootView,"Article Loaded",Snackbar.LENGTH_SHORT);
+        snackbar.show();
     }
 
     @Override
